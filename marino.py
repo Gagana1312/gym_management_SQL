770bb29059f1c3d2ee9f7348a87db883234dbdb6
arps <arpanp99@gmail.com>
2022-12-02 19:31:13 -0500

    Added Staff CRUD

--- marino.py
+++ marino.py
@@ -108,7 +108,68 @@ def admin_session():
             if command_handler.rowcount < 1: 
                 print("No User found")
 
-        elif user_option == "6":
+        # if user_option == "7":
+        #     print("")
+        #     print("Update exisitng Staff")
+        #     # idstaff = input(str("ID: "))
+        #     emailid = input(str("Staff emailid: "))
+        #     password = input(str("Staff password: "))
+        #     staff_name = input(str("Staff Name: "))
+        #     staff_age = input(str("Staff Age: "))
+        #     phone_number = input(str("Staff Phone Number: "))
+        #     query_vals = (staff_name,staff_age,phone_number,emailid,password)
+        #     command_handler.execute("Update INTO marino.staff (staff_name,staff_age,phone_number,emailid,password) VALUES (%s,%s,%s,%s,%s)",query_vals)
+        #     db.commit()
+        #     print(emailid + " has been updated in staff")
+        
+        elif user_option == "9":
+            break
+        else:
+            print("Invalid option Selected!")
+    
+def staff_session():
+    while 1:
+        print("Welcome to Staff Panel")
+        print(" ")
+        print("1. Register new user")     
+        print("2. Delete existing user")
+        print("3. Read existing user")
+        print("4. Update existing user")
+        print("9. Logout")
+
+        user_option = input(str("Option : "))
+       
+        if user_option == "1":
+            print("")
+            print("Register New User")
+            # idstaff = input(str("ID: "))
+            emailid = input(str("user emailid: "))
+            password = input(str("user password: "))
+            first_name = input(str("First Name: "))
+            last_name = input(str("Last Name: "))
+            age = input(str("user Age: "))
+            phone_number = input(str("user Phone Number: "))
+            query_vals = (first_name,last_name,age,phone_number,emailid,password)
+            command_handler.execute("INSERT INTO marino.user(first_name,last_name,age,phone_number,emailid,password) VALUES (%s,%s,%s,%s,%s,%s)",query_vals)
+            db.commit()
+            print(first_name + " has been registered as a User")
+        
+       
+        
+        elif user_option == "2":
+            print("")
+            print("Delete Existing User Account")
+            emailid = input(str("Email ID: "))
+            password = input(str("Password: "))
+            query_vals = (emailid,password)
+            command_handler.execute("DELETE FROM user WHERE emailid = %s AND password = %s",query_vals)
+            db.commit()
+            if command_handler.rowcount < 1: 
+                print("User not found")
+            else:
+                print(emailid + " has been deleted!")
+
+        elif user_option == "3":
             print("")
             print("All Existing User Details")
             # emailid = input(str("Email ID: "))
@@ -124,14 +185,14 @@ def admin_session():
             db.commit()
             if command_handler.rowcount < 1: 
                 print("No User found")
-        
+                
         elif user_option == "9":
             break
         else:
-            print("Invalid option Selected!")
-    
+            print("Invaliid Selection!")
 
 
+#Admin Authorization
 def auth_admin():
     print("")
     print("Admin Login")
@@ -145,6 +206,22 @@ def auth_admin():
             print("Incorrect Password!")
     else:
         print("Login Details not recognized!")
+
+#Staff Authorization
+def auth_staff():
+    print("")
+    print("Staff Login")
+    print("")
+    emailid = input(str("Email ID: "))
+    password = input(str("Password: "))
+    if emailid == "arpa@gmail.com":
+        if password == "arpa":
+            staff_session()
+        else:
+            print("Incorrect Password!")
+    else:
+        print("Login Details not recognized!")
+
 
 def main():
     while 1:
